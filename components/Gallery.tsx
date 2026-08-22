1fade4a065f0f360a32ec4aa258861e2276fc239
saagi <sagarreddy.kallem@gmail.com>
2026-08-22 15:48:19 +0530

    first commit

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -9,12 +9,7 @@ import Image from 'next/image';
 
 // Placeholder images - users should replace with their own
 const photos = [
-  { src: 'https://images.unsplash.com/photo-1519741497674-611481863552?w=800', alt: 'Couple photo 1' },
-  { src: 'https://images.unsplash.com/photo-1511285560929-80b456fea0bc?w=800', alt: 'Couple photo 2' },
-  { src: 'https://images.unsplash.com/photo-1606216794074-735e91aa2c92?w=800', alt: 'Couple photo 3' },
-  { src: 'https://images.unsplash.com/photo-1465495976277-4387d4b0b4c6?w=800', alt: 'Couple photo 4' },
-  { src: 'https://images.unsplash.com/photo-1522673607200-164d1b6ce486?w=800', alt: 'Couple photo 5' },
-  { src: 'https://images.unsplash.com/photo-1583939003579-730e3918a45a?w=800', alt: 'Couple photo 6' },
+  { src: '/photos/134223802326419228.jpg', alt: 'Groom' }
 ];
 
 export default function Gallery() {
